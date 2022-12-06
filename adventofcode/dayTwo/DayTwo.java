663c3e8959527431e60bb406c41b4f69e7ea5751
Christopher Kleemann <christopher.kleemann@gmx.de>
2022-12-06 12:21:52 +0100

    more to bla

--- adventofcode/dayTwo/DayTwo.java
+++ adventofcode/dayTwo/DayTwo.java
@@ -9,9 +9,12 @@ public class DayTwo {
         PairService pairService = new PairService();
 
         var list = reader.loadFileToArray("adventofcode/dayTwo/day2");
-        var pairs = pairService.createPairsPerfect(list);
+        var pairsperfect = pairService.createPairsPerfect(list);
+        var pairs = pairService.createPairs(list);
 
-        var totalScore = pairService.calculateTotalScore(pairs);
-        System.out.println("Score: " + totalScore);
+        var totalScore = pairService.calculateTotalScore(pairsperfect);
+        System.out.println("Perfect Score: " + totalScore);
+        totalScore = pairService.calculateTotalScore(pairs);
+        System.out.println("First Score: " + totalScore);
     }
 }
